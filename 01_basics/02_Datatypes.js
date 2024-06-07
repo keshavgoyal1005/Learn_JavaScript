df272611725514cad62d64066d1df344adca9d15
Keshav Goyal <76448603+keshavgoyal1005@users.noreply.github.com>
2024-06-07 16:49:08 +0000

    data_types done

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,30 @@
+"use strict";  // Treat all the JS version as a newer version 
+
+// alert(3+3); //we are using nodejs not the browser 
+
+
+let name = "Keshav" // string 
+let age = 21        // number 
+let isloggedin = true   // boolean
+let isthis              // undefined
+let state =null         // null
+
+//-----> premitive datatypes
+
+// number
+// string = ""
+// bigint
+// boolean = true or false
+// null = standalone value 
+// undefined = 
+// symbol = uniqueness 
+
+
+// -----> Non premitive datatypes
+
+// objects 
+
+
+console.log(typeof null)    // object
+console.log(typeof undefined) // undefined 
+
