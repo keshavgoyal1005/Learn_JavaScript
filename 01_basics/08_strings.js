202aa92fdee01d8b0389f6372004d538c64d7639
Keshav Goyal <76448603+keshavgoyal1005@users.noreply.github.com>
2024-06-11 16:52:42 +0000

    strings update

---
+++ 01_basics/08_strings.js
@@ -0,0 +1,22 @@
+let name = "Keshav";
+let age = 21
+
+console.log("my name is " + name + " my age is " + age ); // Not a good practise
+
+console.log(`my name is ${name} & my age is ${age}`); // Good practise
+
+const gamename = new String("Keshav-KG");
+
+console.log(gamename[0]);
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+
+console.log(gamename.charAt(2));
+console.log(gamename.indexOf('K'));
+
+const newstring = gamename.substring(0 , 4);
+console.log(newstring);
+
+const anotherstring = gamename.slice(-8 , 4);
+console.log(anotherstring);
+
