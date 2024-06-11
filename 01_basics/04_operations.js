61dd46e75c199e8e83a85a23baf12108bed0021f
Keshav Goyal <76448603+keshavgoyal1005@users.noreply.github.com>
2024-06-11 02:49:56 +0000

    Operations done

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,44 @@
+let num = 3;
+let negnum = -num;
+console.log(negnum);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%2);
+console.log(2**3);
+
+
+let str1 = "Helllo";
+let str2 = " Keshav";
+console.log(str1+str2);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + "2" +2);
+console.log(1 + 2 + "2");
+
+
+
+let gamenum = 99;
+++gamenum;
+console.log(gamenum);
+
+let gamenum1 = 100;
+gamenum1++;
+console.log(gamenum1);
+
+let a = 5;
+let b = a++;  // prefix 
+
+console.log("a -> " , a , " b -> " , b);
+
+let x = 5;
+let y = ++x;  // postfix
+
+console.log("x -> " , x , " y -> " , y);
+
+
+
