fc51fec4474e082c48f5a41c7e100ea05f713f26
Keshav Goyal <76448603+keshavgoyal1005@users.noreply.github.com>
2024-06-11 03:00:32 +0000

    comparisions done

---
+++ 01_basics/05_comparision.js
@@ -0,0 +1,28 @@
+console.log(2 == 2);
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+
+console.log("string with number")
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log("null with number")
+
+console.log(null > 0)
+console.log(null < 0)
+console.log(null == 0)
+console.log(null >= 0)
+console.log(null <= 0)
+
+console.log("undefined with number")
+console.log(undefined > 0)
+console.log(undefined < 0)
+console.log(undefined == 0)
+console.log(undefined >= 0)
+console.log(undefined <= 0)
+
+
+// strict check ===
+console.log("Strict Check");
+console.log("2" === 2);      // Here it check the number as well as the datatype
