156d4ecd9ab184ee5e067da539d2545218221840
Keshav Goyal <76448603+keshavgoyal1005@users.noreply.github.com>
2024-06-12 16:50:16 +0000

    learn more about the objects

---
+++ 02_basics/04_object.js
@@ -0,0 +1,70 @@
+// const tinderuser = new Object();
+// Note ; this is a singeltone object 
+
+const tinderuser = {};
+
+tinderuser.id = "123abx";
+tinderuser.name = "Sam";
+tinderuser.isloggedin = true;
+
+// console.log(tinderuser);
+
+
+const regularuse = {
+    email : "keshavdikhani@microsoft.com",
+    fullname: {
+        completename:{
+            firstname : "Keshav",
+            lastname : "Goyal"
+        }
+    }
+}
+
+// console.log(regularuse.fullname.completename.lastname);
+
+
+const obj1 = {
+    1 : "a",
+    2 : "b"
+}
+
+const obj2 = {
+    3 : "c",
+    4 : "d"
+}
+
+const obj3 = {obj1 , obj2};
+// console.log(obj3);
+
+const obj4 = Object.assign(obj1 , obj2);
+// console.log(obj4);
+
+const obj5 = {...obj1 , ...obj2};         //spread method 
+// console.log(obj5);
+
+
+const user = [
+    {
+        id : 1,
+        email: "keshavgoyal@microsoft.com"
+    },
+    {
+        id: 2,
+        email: "keshavgoyal@google.com"
+    },
+    {
+        id: 3,
+        email: "keshavgoyal@apple.com"
+    }
+    
+]
+
+// here we made a array of objects
+
+// console.log(user[1].email);
+
+
+console.log(tinderuser);
+console.log(Object.keys(tinderuser));
+console.log(Object.values(tinderuser));
+console.log(Object.entries(tinderuser));
