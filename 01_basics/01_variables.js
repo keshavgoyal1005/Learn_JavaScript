c306c3ad8ad297130ee101bd2b2d8a7892d203ef
Keshav Goyal <76448603+keshavgoyal1005@users.noreply.github.com>
2024-06-13 02:19:34 +0000

    minor changes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,18 +1,19 @@
-const account_id = 5919174
-let account_email = "kehavdikhani1005@gmail.com"
-var accountpassword = "King123"
-accountcity = "Sikar"   // not a good practise 
+const account_id = 5919174;
+let account_email = "kehavdikhani1005@gmail.com";
+var accountpassword = "King123";
+accountcity = "Sikar"  ; // not a good practise 
 let accountstate;
 
 // account_id = 123     not allowed 
-account_email = "king@gmail.com"
-accountpassword = "kkk123"
-accountcity  = "jaipur"
+account_email = "king@gmail.com";
+accountpassword = "kkk123";
+accountcity  = "jaipur";
 
 
-console.log(account_id)
-console.log(account_email)
-console.table([account_id , account_email , accountpassword , accountcity, accountstate])
+console.log(account_id);
+console.log(account_email);
+console.table([account_id , account_email , accountpassword , accountcity, accountstate]);
+console.log(typeof accountstate);
 
 /*
 preferred not to use Var 
