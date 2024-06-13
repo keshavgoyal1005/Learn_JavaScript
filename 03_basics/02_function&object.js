10bda2ae8cd0817bc3105173d9d27ce49bda2233
Keshav Goyal <76448603+keshavgoyal1005@users.noreply.github.com>
2024-06-13 04:42:58 +0000

    functions with objects and arrays

---
+++ 03_basics/02_function&object.js
@@ -0,0 +1,46 @@
+function calculateCartPrice(...num){     // here the ... will be the rest operator 
+    return num;
+}
+
+// console.log(calculateCartPrice(100 , 200 , 300));
+
+
+function price(num1 , num3 , ...num){
+    return num;
+}
+
+// console.log(price(100 , 200 , 300 , 400 , 500 ));
+
+
+let user = {
+    name : "Keshav",
+    price : 10000,
+    email : "keshavgoyal@microsoft.com"
+}
+
+function handleObject(myobj){
+    console.log(`${myobj.name} purchase a item of worth ${myobj.price} and the email id is ${myobj.email}`)
+}
+
+// handleObject(user);
+
+handleObject({
+    name : "Abhay",
+    price : 20000,
+    email : "keshavgoyal@apple.com"
+})
+
+
+
+
+
+
+const arr = [111 , 222 , 333 , 444];
+
+function returnSecondValue(arr){
+    return arr[1];
+}
+
+console.log(returnSecondValue(arr));
+
+console.log(returnSecondValue([777 , 888 , 999]));
